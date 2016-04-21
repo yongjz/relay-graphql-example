038b8e7d5b2f61e4b8e89cc12a8a1f0d3666ce38
yongjz <yjz_1008@163.com>
2016-04-22 00:06:18 +0800

    first commit

---
+++ data/database.js
@@ -0,0 +1,47 @@
+export class Game {}
+export class HidingSpot {}
+
+var game = new Game();
+game.id = '1';
+
+var hidingSpots = [];
+(function() {
+  var hidingSpot;
+  var indexOfSpotWithTreasure = Math.floor(Math.random() * 9);
+  console.log(indexOfSpotWithTreasure);
+  for (var i = 0; i < 9; i++) {
+    hidingSpot = new HidingSpot();
+    hidingSpot.id = `${i}`;
+    hidingSpot.hasTreasure = (i === indexOfSpotWithTreasure);
+    hidingSpot.testField = i;
+    hidingSpot.hasBeenChecked = false;
+    hidingSpots.push(hidingSpot);
+  }
+})();
+
+var turnsRemaining = 3;
+
+export function checkHidingSpotForTreasure(id) {
+  if (hidingSpots.some(hs => hs.hasTreasure && hs.hasBeenChecked)) {
+    return;
+  }
+  turnsRemaining--;
+  var hidingSpot = getHidingSpot(id);
+  hidingSpot.hasBeenChecked = true;
+}
+
+export function getHidingSpot(id) {
+  return hidingSpots.find(hs => hs.id === id);
+}
+
+export function getGame() {
+  return game;
+}
+
+export function getHidingSpots() {
+  return hidingSpots;
+}
+
+export function getTurnsRemaining() {
+  return turnsRemaining;
+}
